19052a6d0842c5ecad36aaa86f98a547e5a0cd1d
erasels <emanuelbedburdick@yahoo.de>
2023-11-20 09:53:49 +0100

    Fix pack prefix method not appending a colon and update packCards.json

--- logic/transformations.py
+++ logic/transformations.py
@@ -12,8 +12,8 @@ def del_upg(cardName: str) -> str:
 def add_pack_prefix(cardName: str, card_to_pack: dict, keep_upgrade: bool = True) -> str:
     new_prefix = del_prefix(card_to_pack.get(del_upg(cardName)))
     if keep_upgrade:
-        return new_prefix + del_prefix(del_upg(cardName))
-    return new_prefix + del_prefix(cardName)
+        return new_prefix + ":" + del_prefix(del_upg(cardName))
+    return new_prefix + ":" + del_prefix(cardName)
 
 
 def make_ratio(positive: int, total: int) -> str:
