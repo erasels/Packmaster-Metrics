ddefe1b2d52c4491f443c2ef4094d3747684a6ab
erasels <emanuelbedburdick@yahoo.de>
2024-06-29 19:36:36 +0200

    Delete date_to_metrics since it is unused

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from logic.storage import *
 
 # Global dictionary to store sublists based on file paths
 date_to_metrics = {}
-# Dictionairy of packs to cards
 pack_to_cards = {}
 card_to_pack = {}
 
@@ -20,11 +19,13 @@ if __name__ == "__main__":
         date_to_metrics = iterate_directory(metrics_path)
         print(date_to_metrics.keys())
         save_data_to_pickle(data_file_path, date_to_metrics)
+    print("Data is loaded.")
 
     pack_to_cards = load_data_from_json(os.path.join(data_path, "packCards.json"))
     card_to_rarity = load_data_from_json(os.path.join(data_path, "rarities.json"))
     card_to_pack = reverse_and_flatten_dict(pack_to_cards)
 
-    all_data_dict = round_date_keys(date_to_metrics, 0)
+    all_data_dict = single_key_merge(date_to_metrics)
+    del date_to_metrics
     insights.count_average_win_rate_per_card_split_by_rarity(all_data_dict[""], card_to_pack, card_to_rarity)
 
