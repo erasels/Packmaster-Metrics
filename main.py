1d4a4f9c48c908bd43643ae48bbcf5aae44df6cc
Jay <jordanmyer96@gmail.com>
2024-07-17 19:26:52 -0700

    More wording changes/small format tweaks. Add conditional formatting. Rewrite pack by asc

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ if __name__ == "__main__":
         date_to_metrics = iterate_directory(metrics_path)
         print(date_to_metrics.keys())
         save_data_to_pickle(data_file_path, date_to_metrics)
+
     print("Data is loaded.")
 
     pack_to_cards = load_data_from_json(os.path.join(data_path, "packCards.json"))
@@ -31,28 +32,31 @@ if __name__ == "__main__":
 
     all_data = mega_list_merge(date_to_metrics)
     del date_to_metrics
-    update_insights(insights.sum_filtered_packs(all_data))
-    update_insights(insights.count_enabled_expansion_packs(all_data))
+    update_insights(insights.count_win_rates_per_asc(all_data))
+    update_insights(insights.win_rate_by_ascension_and_pack(all_data))
     update_insights(insights.count_pack_picks(all_data))
-    update_insights(insights.count_most_common_players(all_data))
-    update_insights(insights.count_most_common_picked_hats(all_data))
     update_insights(insights.count_pack_victory_rate(all_data))
     update_insights(insights.count_card_pick_rate(all_data, card_to_pack, card_to_rarity))
-    update_insights(insights.count_win_rates_per_asc(all_data))
-    update_insights(insights.count_median_deck_sizes(all_data))
     update_insights(insights.count_average_win_rate_per_card(all_data, card_to_pack, card_to_rarity))
+    update_insights(insights.win_rate_deviation_between_asc(all_data))
+    update_insights(insights.win_rate_deviation_from_average_by_asc(all_data))
+    update_insights(insights.calculate_card_pick_deviation(all_data, card_to_pack, card_to_rarity))
+    update_insights(insights.count_most_common_picked_hats(all_data))
     update_insights(insights.count_win_rate_per_picked_hat(all_data))
+    update_insights(insights.count_median_deck_sizes(all_data))
     update_insights(insights.count_median_turn_length_per_enemy(all_data))
-    update_insights(insights.upgraded_card_win_rate_analysis(all_data))
     update_insights(insights.median_health_before_rest(all_data))
     update_insights(insights.smith_vs_rest_ratio(all_data))
     update_insights(insights.gem_impact_on_win_rate(all_data))
     update_insights(insights.gem_count_vs_win_rate(all_data))
-    update_insights(insights.win_rate_by_ascension_and_pack(all_data))
-    update_insights(insights.win_rate_deviation_between_asc(all_data))
-    update_insights(insights.win_rate_deviation_from_average_by_asc(all_data))
-    update_insights(insights.calculate_card_pick_deviation(all_data, card_to_pack, card_to_rarity))
+    update_insights(insights.upgraded_card_win_rate_analysis(all_data))
+    update_insights(insights.sum_filtered_packs(all_data))
+    update_insights(insights.count_most_common_players(all_data))
+    update_insights(insights.count_enabled_expansion_packs(all_data))
 
+    update_summary_sheet()
+    # delete_all_sheets_except_first()
+    
     del all_data
     gc.collect()
 
